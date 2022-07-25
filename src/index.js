155178e5b1c29f864a8279edfd298341420a33f5
Joaquin Viera <joaquin.viera@outlook.com>
2022-07-25 07:33:52 -0300

    database connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import app from './app.js';
+import { sequelize } from './database/database.js';
 
-const port = 8000;
-app.listen(port);
+async function main() {
+  const port = 8000;
 
-console.log('Server listening on port', port);
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+    app.listen(port);
+    console.log('Server listening on port', port);
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+main();
