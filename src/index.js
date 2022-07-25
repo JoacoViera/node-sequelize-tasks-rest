08f1e388526b79cc499612d169fa0d6c419bd442
Joaquin Viera <joaquin.viera@outlook.com>
2022-07-25 07:07:19 -0300

    server and linter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import app from './app.js'
+import app from './app.js';
 
-const port = 8000
-app.listen(port)
-console.log('Server listening on port', port)
+const port = 8000;
+app.listen(port);
+console.log('Server listening on port', port);
