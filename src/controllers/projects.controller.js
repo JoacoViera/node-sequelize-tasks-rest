07d23cc1628effc2f5a3b2d7159b16ac4c29f2ba
Joaquin Viera <joaquin.viera@outook.com>
2022-08-08 01:29:55 -0300

    get and create projects

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -1,16 +1,25 @@
 import { Project } from '../models/Project.js';
 
-export const getProjects = (req, res) => {
-  res.send('getting project');
+export const getProjects = async (req, res) => {
+  try {
+    const projects = await Project.findAll();
+    res.json(projects);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const createProject = async (req, res) => {
   const { name, priority, description } = req.body;
-  const newProject = await Project.create({
-    name,
-    description,
-    priority,
-  });
-  console.log(newProject);
-  res.send('creating project');
+
+  try {
+    const newProject = await Project.create({
+      name,
+      description,
+      priority,
+    });
+    res.json(newProject);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
